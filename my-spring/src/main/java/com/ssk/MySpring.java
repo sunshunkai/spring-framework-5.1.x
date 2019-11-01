50039ef469c6a63e3d1fd70bbb3d5bd70487af16
sunshunkai <893589257@qq.com>
2019-11-01 17:27:39 +0800

    gradle子模块搭建

--- my-spring/src/main/java/com/ssk/MySpring.java
+++ my-spring/src/main/java/com/ssk/MySpring.java
@@ -1,12 +1,13 @@
 package com.ssk;
 
 import com.ssk.anno.Appconfig;
+import com.ssk.controller.TestController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MySpring {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Appconfig.class);
-
+		final TestController bean = applicationContext.getBean(TestController.class);
 	}
 }
