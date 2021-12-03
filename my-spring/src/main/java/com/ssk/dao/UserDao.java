4fd9edb477d1de54a63cb3815a58450206f4fc18
孙顺凯 <ssk7495@163.com>
2021-12-03 13:35:58 +0800

    AOP

---
+++ my-spring/src/main/java/com/ssk/dao/UserDao.java
@@ -0,0 +1,6 @@
+package com.ssk.dao;/**
+ * @author  惊云
+ * @date  2021/12/2 20:53
+ */
+public interface UserDao {
+}
---
+++ my-spring/src/main/java/com/ssk/model/User.java
@@ -0,0 +1,6 @@
+package com.ssk.model;/**
+ * @author  惊云
+ * @date  2021/12/2 19:07
+ */
+public class User {
+}
